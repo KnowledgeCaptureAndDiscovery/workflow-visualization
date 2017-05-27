4828f1838421f0cd6e4c84b6879801beddd859da
Jingyun Yang <jingyuny@usc.edu>
2017-05-26 17:24:28 -0700

    Added multivariate module and parallel coordinate plot.

---
+++ visualize-result/graphs/multivariate.js
@@ -0,0 +1,248 @@
+var Multivariate = (function(Dashboard, $) {
+
+	/* Private Variables */
+	var module = Dashboard.multivariate = Dashboard.multivariate || {},
+		$div,
+		$dropdown,
+		$menu,
+		data = {},
+		plotTypes = [];
+
+	/* Helper Functions */
+
+	// @brief	converts display string to strings used as div names
+	// @param	str 	input string
+	// @return 	converted string
+	var displayToEncoding = function(str) {
+		return str.toLowerCase().replace(/\s+/g, '-');
+	};
+
+	// @brief	converts div string names to display string
+	// @param	str 	input string
+	// @return	converted string
+	var encodingToDisplay = function(str) {
+		return str.replace(/\w\S*/g, function(txt) {
+			return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+		});
+	};
+
+	// @brief	converts column type name to a format easier to work with
+	// @param	type 	name of a column type
+	// @return	converted column type
+	var getType = function(type) {
+		if(type["type"] == "numeric") {
+			return type["type"];
+		}
+		else {
+			return "others";
+		}
+	};
+
+	/* Member Functions */
+
+	// @brief	initialization code
+	// @param	renderTo	the jQuery div object 
+	//						that the module is rendered to
+	// @param	dataCopy	the data used in the visualization 
+	//						(data is shallow copied)
+	module.init = function(renderTo, dataCopy) {
+		$div = renderTo;
+		data = dataCopy;
+		$dropdown = $div.find(".col-dropdown").find(".fluid.dropdown");
+		$menu = $div.find(".has-menu").find(".menu");
+
+		module.initPlotTypes();
+
+		var availableIx = module.initGraphOptionDropdown();
+
+		if(availableIx.length >= 2) {
+			// define dropdown action events
+			$div.find(".col-dropdown").find(".fluid.dropdown").dropdown("setting", "onChange", function() {
+				module.render();
+			});
+
+			availableIx.forEach(function(item) {
+				$dropdown.dropdown("set selected", item);
+			});
+
+			module.render();
+
+			$div.removeClass("hidden");
+		}
+		else {
+			console.log("No multivariate graph to show");
+			$div.addClass("hidden");
+		}
+	};
+
+	// @brief	extract plot types from html and add click event handlers
+	module.initPlotTypes = function() {
+		$menu.find("a.item").each(function() {
+			plotTypes.push(displayToEncoding($(this).html()));
+
+			$(this).click(function() {
+				var shownItem = displayToEncoding($(this).html());
+
+				$menu.find(".active.item").removeClass("active");
+				$(this).addClass("active");
+				
+				plotTypes.forEach(function(type) {
+					$div.find("." + type).addClass("hidden");
+				});
+				$div.find("." + shownItem).removeClass("hidden");
+			});
+		});
+	};
+
+	// @brief	populate graph option dropdown based on column data types
+	// @return	column indices that are shown in the dropdown
+	module.initGraphOptionDropdown = function() {
+		var columnOptions = "";
+		var availableIx = [];
+		for(ix in data["attribute"]) {
+			var type = data["attribute"][ix]["type"];
+			if(getType(type) != "others") {
+				// set first available index if necessary
+				availableIx.push(ix);
+
+				// populate column selection dropdown
+				$dropdown.append($("<option>")
+					.attr("value",ix)
+					.text(data["attribute"][ix]["name"]));
+			}
+		}
+		return availableIx;
+	};
+
+	// @brief	delete bad data in a numeric data column
+	//			add bad data notice in the module
+	// @param	columnData	the column data to be trimmed
+	// @param	types 		data types of the 2 columns
+	// @return	trimmed column data
+	module.trimBadData = function(columnData, types) {
+		var $missingNotice = $div.find(".missing-notice");
+
+		if(types.indexOf("numeric") == -1) {
+			$missingNotice.addClass("hidden");	
+		}
+
+		else {
+			columnData = d3.transpose(columnData);
+
+			var originalLength = columnData.length;
+
+			for(var ix = 0; ix < types.length; ix++) {
+				columnData = columnData.filter(function(val) {
+					return (typeof val[ix] === 'number');
+				});
+			}
+
+			var trimmedLength = columnData.length;
+
+			if(trimmedLength != originalLength) {
+				$missingNotice.html(
+					"<i class='warning sign icon'></i> " + (originalLength - trimmedLength) 
+						+ " of " + originalLength 
+						+ " data points are identified as problematic data and are omitted."
+				);
+				$missingNotice.removeClass("hidden");
+			}
+			else {
+				$missingNotice.addClass("hidden");
+			}
+
+			columnData = d3.transpose(columnData);
+		}
+
+		return columnData;
+	};
+
+	// @brief	init modules and set module visibility
+	module.render = function() {
+		var ix = $dropdown.dropdown("get value").slice(0,-1);
+		var types = ix.map(function(val) { return getType(data["attribute"][val]["type"]); });
+
+		var columnData = ix.map(function(val) { return module.getColumn(val); });
+		columnData = module.trimBadData(columnData, types);
+
+		// obtain modules that needs rendering
+		var names = [];
+		if(types.find(function(type) { return type != "numeric"; }) == null) {
+			names = ["parallel-coordinate"];
+		}
+		else {
+			return;
+		}
+
+		// initialize necessary modules
+		names.forEach(function(name) {
+			// TIME_KEEPING
+			// console.log("Bivariate " + name + " Starts: " + ((new Date())-window.ST));
+
+			var moduleName = name.replace(/-/g,"");
+			var $divToRender = $div.find("." + name);
+			var columnNames = ix.map(function(val) { return data["attribute"][val]["name"] });
+			if(types.find(function(type) { return type != "numeric"; }) == null) {
+				module[moduleName].init(
+					$divToRender, columnNames, columnData
+				);
+			}
+
+			// TIME_KEEPING
+			// console.log("Bivariate " + name + " Ends: " + ((new Date())-window.ST));
+		});
+
+		// toggle show and hide
+		plotTypes.forEach(function(type) {
+			var typeName = encodingToDisplay(type.replace(/-/g, " "));
+			if(names.indexOf(type) != -1) {
+				$div.find("." + type).removeClass("hidden");
+				$menu.find("a.item:contains('" + typeName + "')").removeClass("hidden");
+			}
+			else {
+				$div.find("." + type).addClass("hidden");
+				$menu.find("a.item:contains('" + typeName.replace(/-/g, " ") + "')").addClass("hidden");
+			}
+		});
+
+		// set tab status
+		var $activeItem = $menu.find(".active.item");
+		if(names.indexOf($activeItem.html().toLowerCase()) == -1) {
+			var typeToActivate = names[0];
+			var firstVisibleType = encodingToDisplay(typeToActivate.replace(/-/g, " "));
+			$menu.find("a.item:contains('" + firstVisibleType + "')").click();
+		}
+		else {
+			$activeItem.click();
+		}
+	};
+
+	module.getColumn = function(ix) {
+		return data["data"].map(function(val) {
+			return val[data["attribute"][ix]["name"]];
+		});
+	};
+
+	// @brief	reset the module
+	module.reset = function() {
+		// if init has not been run, do nothing
+		if(!$div) return;
+
+		data = {};
+		$div.find(".col-dropdown").html(""
+			+ "<select multiple='' class='ui fluid dropdown'>"
+			+   "<option value=''>Select Columns</option>"
+			+ "</select>"
+		);
+
+		plotTypes.forEach(function(name) {
+			name = name.replace(/-/g,"");
+			if(module[name]) {
+				module[name].reset();
+			}
+		});
+	};
+
+	return module;
+
+}(Dashboard || {}, jQuery));
---
+++ visualize-result/graphs/multivariate/parallelcoordinate.js
@@ -0,0 +1,188 @@
+Multivariate.parallelcoordinate = (function($) {
+	var module = {},
+		$div,
+		$graph,
+		dimensions = [],
+		data = [],
+		resizeTimeout = false,
+		rtime;
+
+	module.init = function(renderTo, seriesNames, dataCopy) {
+		module.reset();
+
+		$div = renderTo;
+		dimensions = seriesNames;
+		data = d3.transpose(dataCopy);
+
+		$graph = $div.find('.chart.image');
+
+		module.render();
+
+		window.addEventListener("resize", function() {
+			var delta = 120;
+			function checkResizeEnd() {
+	        	if (new Date() - rtime < delta) {
+			        setTimeout(checkResizeEnd, delta);
+			    } else {
+			        resizeTimeout = false;
+			        module.render();
+			    }  
+			}
+			rtime = new Date();
+			if (resizeTimeout === false) {
+		        resizeTimeout = true;
+		        setTimeout(checkResizeEnd, delta);
+		    }
+		});
+	};
+
+	// Code adapted from https://bl.ocks.org/jasondavies/1341281
+	module.render = function() {
+		$graph.html("");
+
+		var margin = {top: 30, right: 10, bottom: 10, left: 10},
+			width = $graph.width() - margin.left - margin.right,
+			height = 400 - margin.top - margin.bottom;
+
+		var x = d3.scalePoint().domain(dimensions).range([0, width]).padding(0.5),
+			y = {},
+			dragging = {};
+
+		dimensions.forEach(function(item, ix) {
+			y[item] = d3.scaleLinear()
+				.domain(d3.extent(data, function(p) { return +p[ix]; }))
+				.range([height, 0]);
+		});
+
+		var line = d3.line(),
+			axis = d3.axisLeft(),
+			background,
+			foreground;
+
+		var svg = d3.select($graph.get(0)).append("svg")
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom)
+		  .append("g")
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+		// Add grey background lines for context.
+		background = svg.append("g")
+			.attr("class", "background")
+		  .selectAll("path")
+			.data(data)
+		  .enter().append("path")
+			.attr("d", path);  
+
+		// Add blue foreground lines for focus.
+		foreground = svg.append("g")
+			.attr("class", "foreground")
+		  .selectAll("path")
+			.data(data)
+		  .enter().append("path")
+			.attr("d", path);
+
+		// Add a group element for each dimension.
+		var g = svg.selectAll(".dimension")
+			.data(dimensions)
+		  .enter().append("g")
+			.attr("class", "dimension")
+			.attr("transform", function(d) { return "translate(" + x(d) + ")"; });
+			// .call(d3.drag()
+			// .subject(function(d) { return {x: x(d)}; })
+			// .on("start", function(d) {
+			// 	dragging[d] = x(d);
+			// 	background.attr("visibility", "hidden");
+			// })
+			// .on("drag", function(d) {
+			// 	dragging[d] = Math.min(width, Math.max(0, d3.event.x));
+			// 	foreground.attr("d", path);
+			// 	dimensions.sort(function(a, b) { return position(a) - position(b); });
+			// 	x.domain(dimensions);
+			// 	g.attr("transform", function(d) { return "translate(" + position(d) + ")"; })
+			// })
+			// .on("end", function(d) {
+			// 	delete dragging[d];
+			// 	transition(d3.select(this)).attr("transform", "translate(" + x(d) + ")");
+			// 	transition(foreground).attr("d", path);
+			// 	background
+			// 		.attr("d", path)
+			// 		.transition()
+			// 		.delay(500)
+			// 		.duration(0)
+			// 		.attr("visibility", null);
+			// }));
+
+		// Add an axis and title.
+		g.append("g")
+		  .attr("class", "axis")
+		  .each(function(d) { d3.select(this).call(axis.scale(y[d])); })
+		.append("text")
+		  .style("text-anchor", "middle")
+		  .attr("y", -9)
+		  .html(function(d) { return '<tspan>' + d + '</tspan>'; })
+		  .style("font-size", "12px")
+		  .style("font-family", "'Lato', sans-serif");
+
+		// Add and store a brush for each axis.
+		g.append("g")
+			.attr("class", "brush")
+			.each(function(d) {
+				d3.select(this).call(y[d].brush = d3.brushY()
+					.extent([[-10,0], [10,height]])
+					.on("start", brushstart)
+					.on("brush", brush)
+					.on("end", brush));
+			})
+		.selectAll("rect")
+			.attr("x", -8)
+			.attr("width", 16);
+
+		function position(d) {
+			var v = dragging[d];
+			return v == null ? x(d) : v;
+		}
+
+		function transition(g) {
+			return g.transition().duration(500);
+		}
+
+		// Returns the path for a given data point.
+		function path(d) {
+			return line(dimensions.map(function(p, ix) { return [position(p), y[p](d[ix])]; }));
+		}
+
+		function brushstart() {
+			d3.event.sourceEvent.stopPropagation();
+		}
+
+		// Handles a brush event, toggling the display of foreground lines.
+		function brush() {
+			var actives = {};
+			svg.selectAll(".brush")
+				.filter(function(p) { return d3.brushSelection(this); })
+				.each(function(p) { actives[p] = d3.brushSelection(this); });
+			console.log(actives);
+			foreground.style("display", function(d) {
+				var count = 0;
+				Object.keys(actives).forEach(function(p) {
+					var ix = dimensions.indexOf(p);
+					if(!(actives[p][0] <= y[p](d[ix]) && y[p](d[ix]) <= actives[p][1])) {
+						count++;
+					}
+				});
+				return (count == 0) ? null : "none";
+			});
+		}
+	};
+
+	module.reset = function() {
+		// if init has not been run, do nothing
+		if(!$div) return;
+
+		data = [];
+		$graph.html("");
+	};
+
+	return module;
+	
+}(jQuery));
